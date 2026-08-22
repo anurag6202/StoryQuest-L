a54a2a786b47666f34241be14cbf5932e29594f3
Anurag Nath <anurag26202@gmail.com>
2026-08-22 21:38:35 +0530

    feat: add explore and game details routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Explore from "./pages/Explore";
+import GameDetails from "./pages/GameDetails";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      <Route
+        path="/explore"
+        element={<Explore />}
+      />
+
+      <Route
+        path="/game/:id"
+        element={<GameDetails />}
+      />
     </Routes>
   );
 }
